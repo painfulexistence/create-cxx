337a5f77798971737a97d13334fcef42a141be05
loic <910041@gmail.com>
2024-04-06 14:45:07 +0800

    feat: Automatically update project name and version

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,27 @@ program.parse();
 const defaultProjectName = "my-cxx-project";
 const cwd = process.cwd();
 const projectName = program.args[0] || defaultProjectName;
+const projectVersion = "0.1.0";
 const { template } = program.opts();
+
+function updateCmakeProjectDesc(filePath: string, name: string, version: string) {
+    const text = fs.readFileSync(filePath, "utf-8");
+    const regex = /project\((.*)\)/;
+    const match = regex.exec(text);
+    if (match) {
+        const newText = text.replace(match[0], `project(${name} VERSION ${version})`);
+        fs.writeFileSync(filePath, newText);
+    }
+}
+
+function updateVcpkgProjectDesc(filePath: string, name: string, version: string) {
+    const text = fs.readFileSync(filePath, "utf-8");
+    const data = JSON.parse(text);
+    data.name = name;
+    data.version = version;
+    const newText = JSON.stringify(data, null, 2);
+    fs.writeFileSync(filePath, newText);
+}
 
 async function run() {
     // TODO: Use Inquirer to prompt users
@@ -41,6 +61,12 @@ async function run() {
             fs.renameSync(oldPath, newPath);
         }
     })
+
+    // Update CMakeLists.txt and vcpkg.json
+    const cmakeFilePath = path.join(projectDir, "CMakeLists.txt");
+    updateCmakeProjectDesc(cmakeFilePath, projectName, projectVersion);
+    const vcpkgFilePath = path.join(projectDir, "vcpkg.json");
+    updateVcpkgProjectDesc(vcpkgFilePath, projectName, projectVersion);
 
     // Closing message
     console.log(chalk.green("Done! ") + " Now run: \n");
