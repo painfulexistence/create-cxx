d3670c9371b232ea1c42c17047cfe67641ab9e29
loic <910041@gmail.com>
2024-05-09 23:21:55 +0800

    WIP: add ogre template

---
+++ templates/ogre/src/main.cpp
@@ -0,0 +1,10 @@
+#include "Ogre.h"
+#include "OgreApplicationContext.h"
+#include "fmt/core.h"
+
+int main(int argc, char* args[]) {
+    fmt::print("Hello from CXX!\n");
+    // OgreBites::ApplicationContext ctx("MyApp");
+    // ctx.initApp();
+    return 0;
+}
